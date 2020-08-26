29dc7e4b4d19f9095ef601a343db4db21ebf2be5
danny galimi <dannygalimi13@aim.com>
2020-08-26 22:43:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,11 +28,11 @@ def adding
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
+  }
                 shipping_manifest{"muskets" => 2}
 
   
  shipping_manifest{"gun powder" => 4}
-}
-  # return the shipping_manifest hash below
+} # return the shipping_manifest hash below
 shipping_manifest
 end
